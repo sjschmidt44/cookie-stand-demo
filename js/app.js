b247cb3265e5153679c4e0880814c9dc64751f5f
Scott Schmidt <sjschmidt44@gmail.com>
2015-11-13 15:19:42 -0800

    Update object literal with corrections to generateHourly method, currently working.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
-var shops = [];
+var shops = [pikePlace];
 
 var pikePlace = {
   minCustomer: 7,
@@ -9,15 +9,16 @@ var pikePlace = {
   avgCustomer: 2.5,
   hourlyCookies: [],
   dailyCookies: 0,
-  shops.push(this)
   
   randCustomer: function(min, max) {
     return Math.random() * (max - min) + min;
   },
 
   generateHourly: function() {
-    for (h in hours) {
-      hourlyCookies = Math.floor(this.avgCustomer * this.randCustomer());
+    for (var i = 0; i < hours.length; i++) {
+      var cookie = Math.floor(this.avgCustomer * this.randCustomer(this.minCustomer, this.maxCustomer));
+      this.hourlyCookies = cookie;
+      this.dailyCookies += cookie;
     }
   }
 }
