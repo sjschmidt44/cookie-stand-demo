a4015a137036828d298ae35e08ad33f39f18a01f
Scott Schmidt <sjschmidt44@gmail.com>
2015-11-13 15:31:07 -0800

    Update render method of first object; now displays generated random hourly data to the DOM.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
 var shops = [pikePlace];
 
 var pikePlace = {
+  name: 'Pike Place',
   minCustomer: 7,
   maxCustomer: 18,
   avgCustomer: 2.5,
@@ -17,8 +18,22 @@ var pikePlace = {
   generateHourly: function() {
     for (var i = 0; i < hours.length; i++) {
       var cookie = Math.floor(this.avgCustomer * this.randCustomer(this.minCustomer, this.maxCustomer));
-      this.hourlyCookies = cookie;
+      this.hourlyCookies.push(cookie);
       this.dailyCookies += cookie;
+    }
+  },
+
+  render: function() {
+    this.generateHourly();
+
+    var ulEl = document.createElement('ul');
+    ulEl.appendChild(document.createTextNode(this.name));
+    var sectionEl = document.getElementById('store_data').appendChild(ulEl);
+
+    for (var i = 0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ": " + this.hourlyCookies[i];
+      ulEl.appendChild(liEl);
     }
   }
 }
