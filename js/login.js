31a9d0656271cb5133db02ebfb3498244f345f89
Scott Schmidt <sjschmidt44@gmail.com>
2015-11-17 11:23:30 -0800

    Update styleguide with minor changes to heading tags; Add basic content to home page; Create login.js file for mock-up login

---
+++ js/login.js
@@ -0,0 +1,10 @@
+(function() {
+  var login = document.getElementById('login');
+  login.addEventListener('click', function(event) {
+    //on click, present the user with popup
+    //user will log in.
+    //user will receive second popup
+    //user will select from stylesheet or sales 
+    //user will be redirected to selected page
+  })
+})()
